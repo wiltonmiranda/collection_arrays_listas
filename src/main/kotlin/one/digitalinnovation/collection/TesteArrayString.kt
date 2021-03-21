b360e391c401e77df4b7ff2935c02bbdabc55211
wiltonmiranda <wilton_m_s@hotmail.com>
2021-03-21 20:56:05 -0300

    Criado teste de arrayString.

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteArrayString.kt
@@ -0,0 +1,24 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val nomes = Array(3) {""}
+
+    nomes[0] = "Maria"
+    nomes[1] = "Zazá"
+    nomes[2] = "José"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println("---------------")
+
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+    println("---------------")
+
+    val nomes2 = arrayOf("Maria", "Zazá", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
