8e1b68f50dcc30bc497ab6cf31aa051ddffbca18
wiltonmiranda <wilton_m_s@hotmail.com>
2021-03-25 21:16:33 -0300

    Atualizando arquivo TesteList, extraíndo dados para criar arquivos Funcionario e TesteSet.

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteSet.kt
@@ -0,0 +1,26 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios1 = setOf(joao, pedro)
+    val funcionarios2 = setOf(maria)
+
+    val resultUnion = funcionarios1.union(funcionarios2)
+    resultUnion.forEach { println(it) }
+
+    println("-------------------------")
+    val funcionarios3 = setOf(joao, pedro, maria)
+    val resultSubtract = funcionarios3.subtract(funcionarios2)
+    resultSubtract.forEach { println(it) }
+
+    println("-------------------------")
+    val resultIntersect = funcionarios3.intersect(funcionarios2)
+    resultIntersect.forEach { println(it) }
+
+
+
+
+}
